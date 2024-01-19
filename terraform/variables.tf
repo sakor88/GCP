5456b4490f11363c2b8280f3772fd8af3ded10a5
Krach <krachuu64@gmail.com>
2024-01-19 04:10:43 +0100

    added variables

---
+++ terraform/variables.tf
@@ -0,0 +1,12 @@
+variable "credentials_file" {
+  default = "account.json"
+}
+
+
+variable "project_id" {
+  default = "gcp-final-project-410222"
+}
+
+variable "region" {
+  default = "europe-west2"
+}
